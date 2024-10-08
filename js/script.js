97f4ab591a56262175d691377a3158cfeb311883
Roni <rhroni09@gmail.com>
2024-10-09 02:30:52 +0600

    final touch on functionality

--- js/script.js
+++ js/script.js
@@ -9,8 +9,15 @@ document.querySelector(".check").addEventListener("click", function () {
   document.querySelector(".dot").value = 8;
   const guess = Number(document.querySelector(".select").value);
 
-  if (guess === nextBowl) {
+  if (!guess) {
+    document.querySelector(".message").textContent =
+      "⛔ please  input a valid number";
+  } else if (guess === nextBowl) {
     document.querySelector(".message").textContent = "🥳 Hurray! You won..";
+    document.querySelector(".main").style.backgroundColor = "#37b24d";
+    document.querySelector("body").style.backgroundColor = "#fa5252";
+    document.querySelector(".message").style.fontSize = "3rem";
+    document.querySelector(".message").style.color = "#9c36b5";
     if (score > highScore) {
       highScore = score;
       document.querySelector(".high-score").textContent = highScore;
@@ -20,7 +27,7 @@ document.querySelector(".check").addEventListener("click", function () {
       if (guess < 7 && guess > nextBowl) {
         document.querySelector(".message").textContent =
           "🐸 Your guess is too high ! 📈";
-      } else if (guess < 7 && guess < nextBowl) {
+      } else if (guess < 7 && nextBowl < 6 && guess < nextBowl) {
         document.querySelector(".message").textContent =
           "🐸 Your guess is too low ! 📉";
       } else {
@@ -35,4 +42,15 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "🚫 You lost the Game..";
     document.querySelector(".score").textContent = 0;
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector(".select").value = "Please choose one :";
+  document.querySelector(".score").textContent = 10;
+  document.querySelector(".message").textContent = "Start Guessing..";
+
+  document.querySelector(".main").style.backgroundColor = "#fff9db";
+  document.querySelector("body").style.backgroundColor = "#fcc419";
+  document.querySelector(".message").style.fontSize = "2.4rem";
+  document.querySelector(".message").style.color = "#fa5252";
 });
