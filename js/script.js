b604299c60a9a1571b189fdfbc12d387693d5747
Roni <rhroni09@gmail.com>
2024-10-30 00:25:07 +0600

    Refactor code and solve few bugs

--- js/script.js
+++ js/script.js
@@ -1,60 +1,86 @@
 "use strict";
+// Selectors
+const wicketEl = document.querySelector(".wicket");
+const dotEl = document.querySelector(".dot");
+const messageEl = document.querySelector(".message");
+const mainEl = document.querySelector(".main");
+const bodyEl = document.querySelector("body");
+const highScoreEl = document.querySelector(".high-score");
+const scoreEl = document.querySelector(".score");
+const selectEl = document.querySelector(".select");
+const btnCheck = document.querySelector(".check");
+const btnAgain = document.querySelector(".again");
+
+// Hiding element
+const hidden = function () {
+  btnCheck.classList.add("hidden");
+};
+
+// Starting stage
 let score = 10;
 let highScore = 0;
-const nextBowl = Math.trunc(Math.random() * 8 + 1);
+const nextBowl = Math.trunc(Math.random() * 8 + 1); // 1-8
 
-document.querySelector(".check").addEventListener("click", function () {
-  document.querySelector(".wicket").value = 7;
-  document.querySelector(".dot").value = 8;
-  const guess = Number(document.querySelector(".select").value);
+// Selecting Check Button
+btnCheck.addEventListener("click", function () {
+  // Setting value for wicket and dot (1-6 for score 7-8 for wicket and dot)
+  wicketEl.value = 7;
+  dotEl.value = 8;
 
+  const guess = Number(selectEl.value);
+
+  // If user doesn't guess anything
   if (!guess) {
-    document.querySelector(".message").textContent =
-      "⛔ please  input a valid number";
+    messageEl.textContent = "⛔ please  input a valid number";
+
+    // If user guess it right
   } else if (guess === nextBowl) {
-    document.querySelector(".message").textContent = "🥳 Hurray! You won..";
-    document.querySelector(".main").style.backgroundColor = "#37b24d";
-    document.querySelector("body").style.backgroundColor = "#fa5252";
-    document.querySelector(".message").style.fontSize = "3rem";
-    document.querySelector(".message").style.color = "#9c36b5";
+    messageEl.textContent = "🥳 Hurray! You won..";
+    mainEl.style.backgroundColor = "#37b24d";
+    bodyEl.style.backgroundColor = "#fa5252";
+    messageEl.style.fontSize = "3rem";
+    messageEl.style.color = "#9c36b5";
+    hidden();
+
     if (score > highScore) {
       highScore = score;
-      document.querySelector(".high-score").textContent = highScore;
+      highScoreEl.textContent = highScore;
     }
+
+    // If user guess it wrong
   } else if (guess !== nextBowl) {
     if (score > 1) {
       if (guess < 7 && guess > nextBowl) {
-        document.querySelector(".message").textContent =
-          "🐸 Your guess is too high ! 📈";
+        messageEl.textContent = "🐸 Your guess is too high ! 📈";
       } else if (guess < 7 && nextBowl < 6 && guess < nextBowl) {
-        document.querySelector(".message").textContent =
-          "🐸 Your guess is too low ! 📉";
+        messageEl.textContent = "🐸 Your guess is too low ! 📉";
       } else {
-        document.querySelector(".message").textContent =
-          "🐸 Your guess is wrong ! 📉";
+        messageEl.textContent = "🐸 Your guess is wrong ! 📉";
       }
 
       score--;
-      document.querySelector(".score").textContent = score;
+      scoreEl.textContent = score;
     } else {
-      document.querySelector(".message").textContent = "🚫 You lost the Game..";
-      document.querySelector(".score").textContent = 0;
-      document.querySelector(".main").style.backgroundColor = "#111";
-      document.querySelector("body").style.backgroundColor = "#e03131";
-      document.querySelector(".message").style.fontSize = "3rem";
-      document.querySelector(".message").style.color = "#9c36b5";
+      messageEl.textContent = "🚫 You lost the Game..";
+      scoreEl.textContent = 0;
+      mainEl.style.backgroundColor = "#111";
+      bodyEl.style.backgroundColor = "#e03131";
+      messageEl.style.fontSize = "3rem";
+      messageEl.style.color = "#9c36b5";
+      hidden();
     }
   }
 });
 
-document.querySelector(".again").addEventListener("click", function () {
-  document.querySelector(".select").value = "Please choose one :";
+// Slecting the Play again button
+btnAgain.addEventListener("click", function () {
+  selectEl.value = "Please choose one :";
   score = 10;
-  document.querySelector(".score").textContent = 10;
-  document.querySelector(".message").textContent = "Start Guessing..";
-
-  document.querySelector(".main").style.backgroundColor = "#fff9db";
-  document.querySelector("body").style.backgroundColor = "#fcc419";
-  document.querySelector(".message").style.fontSize = "2.4rem";
-  document.querySelector(".message").style.color = "#fa5252";
+  scoreEl.textContent = 10;
+  messageEl.textContent = "Start Guessing..";
+  mainEl.style.backgroundColor = "#fff9db";
+  bodyEl.style.backgroundColor = "#fcc419";
+  messageEl.style.fontSize = "2.4rem";
+  messageEl.style.color = "#fa5252";
+  btnCheck.classList.remove("hidden");
 });
